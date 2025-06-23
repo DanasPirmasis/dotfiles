c033e073375cd259af3192f66e883e14f770d891
DanasPirmasis <alisauskas53@gmail.com>
2025-06-23 09:12:20 +0300

    nvim kitty changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
+require("kitty-config")
 vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = ' '
---
+++ .config/nvim/lua/kitty-config.lua
@@ -0,0 +1,20 @@
+-- Create a group to keep things clean
+local group = vim.api.nvim_create_augroup("KittyConfig", { clear = true })
+
+-- Remove padding on Neovim start
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group,
+  callback = function()
+    os.execute("kitty @ set-spacing padding=0")
+    os.execute("kitty @ set-font-size 12")
+  end,
+})
+
+-- Reset padding on Neovim exit
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  group = group,
+  callback = function()
+    os.execute("kitty @ set-spacing padding=25")
+    os.execute("kitty @ set-font-size 10")
+  end,
+})
