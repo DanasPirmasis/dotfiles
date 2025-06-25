f19a51ef3add4b079a90beb8784ce4b639af2ee8
DanasPirmasis <alisauskas53@gmail.com>
2025-06-25 17:42:22 +0300

    json formating and waybar changes

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,20 @@
 return {
 	"stevearc/conform.nvim",
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true }, function(err, did_edit)
+					if not err and did_edit then
+						vim.notify("Code formatted", vim.log.levels.INFO, { title = "Conform" })
+					end
+				end)
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer",
+		},
+	},
 	opts = {
 		format_on_save = {
 			timeout_ms = 500,
@@ -11,6 +26,11 @@ return {
 			go = { "goimports", "golines", "gofmt" },
 			javascript = { "prettier", stop_after_first = true },
 			typescript = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
 		},
+		init = function()
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
 	},
 }
